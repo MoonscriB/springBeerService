925faf48bb549128b8e0264477db6267719e96ec
YannickBeriault <beriault.yannick@gmail.com>
2022-07-22 14:56:49 -0400

    Project initialized.

---
+++ src/main/java/microServicesPractice/sFGuru/springBeerService/SpringBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package microServicesPractice.sFGuru.springBeerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microServicesPractice/sFGuru/springBeerService/SpringBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microServicesPractice.sFGuru.springBeerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
